e94c2f9302b04bff5ff702d375a6994e41f7bf64
Abhishek mishra <44899517+akm0786@users.noreply.github.com>
2020-06-23 09:13:56 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,17 +1,28 @@
 //factorial program for n number
+
 #include <stdio.h>
 
-int main()
-{
-    int num , i,fact=1;
-    printf("enter the number for factorial ");
-    
-    scanf("%d",&num);
-    for(i=1;i<=num;i++)
+       int factorial(int User_inputF)
+        {
+            if(User_inputF==0)
+            {
+                return 1;
+            }
+            else
+            {
+                return (User_inputF*factorial(User_inputF-1));    
+            }
+        }
+        
+    int main()
     {
-        fact=fact*i;
-    }
-    printf("%d!=  %d",num,fact);
- 
+        int User_input;
+        printf("Enter any number");
+        scanf("%d",&User_input);
+        int result=factorial(User_input);
+      
+        printf("Factorial of %d number is %d",User_input,result );
+    
+
     return 0;
 }
